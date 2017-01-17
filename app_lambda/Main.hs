2b224070ef42b668b42eb2aa0fbf04790ded4038
natebragg <nate@cs.tufts.edu>
2017-01-17 09:56:58 -0500

    Put main loop inside forever instead of using recursion.

--- app_lambda/Main.hs
+++ app_lambda/Main.hs
@@ -5,12 +5,14 @@ import DynSem (eval)
 
 import Text.Parsec (parse)
 import System.IO (hFlush, stdout)
+import Control.Monad (forever)
 
 main :: IO ()
-main = do putStr "-> "
-          hFlush stdout
-          inp <- getLine
-          case parse term "" inp of
-            Left err -> putStrLn $ "error: " ++ show err
-            Right t -> putStrLn $ show $ eval t
-          main
+main = do
+          forever $ do
+            putStr "-> "
+            hFlush stdout
+            inp <- getLine
+            case parse term "" inp of
+              Left err -> putStrLn $ "error: " ++ show err
+              Right t -> putStrLn $ show $ eval t

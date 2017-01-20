94d1f01646eff83233c59e4172cb7ea33d9db7ab
natebragg <nate@cs.tufts.edu>
2017-01-20 09:57:57 -0500

    Break evaluators out of semantics, and add a step-evaluator.

--- app_lambda/DynSem.hs
+++ app_lambda/DynSem.hs
@@ -6,63 +6,62 @@ module DynSem (
 
 import Ast (Name, Term(..))
 import Redex (RedexT, runRedexT, reduce, term, (<|>))
-import Control.Monad.State (StateT, evalStateT, runStateT, get, put)
-
-import Data.Maybe (fromJust, isNothing)
+import Control.Monad.State (StateT, evalStateT, get, put)
+import Control.Monad.Trans.Maybe (MaybeT, runMaybeT)
+import Control.Monad.Cont (ContT(..), runContT)
 
 type FreshName = Int
 
-type Lambda = RedexT Term (StateT FreshName Maybe)
+type Lambda m = RedexT Term (MaybeT (StateT FreshName m))
 
-untilS :: (a -> StateT s Maybe a) -> a -> StateT s Maybe a
-untilS f t = do v <- get
-                case runStateT (f t) v of
-                  Nothing -> return t
-                  Just (t', v') -> put v' >> untilS f t'
+potred :: (Monad m) => Term -> ContT Term (StateT FreshName m) Term
+potred t = ContT $ \hook -> do
+    t' <- runMaybeT $ runRedexT redex t
+    maybe (return t) ((>>= flip runContT hook . potred) . hook) t'
 
-eval :: Term -> Term
-eval t = fromJust $ evalStateT (untilS (runRedexT redex) t) 0
+eval :: (Monad m) => Term -> (Term -> StateT FreshName m Term) -> m Term
+eval t h = evalStateT (runContT (potred t) h) 0
 
 freein :: Name -> Term -> Bool
 x `freein` X y = x == y
 x `freein` App t_1 t_2 = x `freein` t_1 || x `freein` t_2
 x `freein` Lam y t = x /= y && x `freein` t
 
-fresh :: Lambda Name
+fresh :: (Monad m) => Lambda m Name
 fresh = do var <- get
            put (var + 1)
            return $ "a" ++ show var
 
-subst :: Term -> Lambda Term -> Lambda Term
+subst :: (Monad m) => Term -> Lambda m Term -> Lambda m Term
 subst = flip reduce
 
 infix 8 `subst`
 
-cong0 :: Lambda Term
+cong0 :: (Monad m) => Lambda m Term
 cong0 = -- [t_1 t_2' | t_1 t_2 <- t, t_2' <- reduce t_2]
         [App t_1 t_2' | App t_1 t_2 <- term, t_2' <- reduce redex t_2]
 
-cong1 :: Lambda Term
+cong1 :: (Monad m) => Lambda m Term
 cong1 = -- [t_1' t_2 | t_1 t_2 <- t, t_1' <- reduce t_1]
         [App t_1' t_2 | App t_1 t_2 <- term, t_1' <- reduce redex t_1]
 
-beta :: Lambda Term
+beta :: (Monad m) => Lambda m Term
 beta = -- [ v[x|->t'] | (\x.v) t' <- t]
         [ t'' | App (Lam x v) t' <- term, t'' <- v `subst` x|->t']
 
-cong2 :: Lambda Term
+cong2 :: (Monad m) => Lambda m Term
 cong2 = -- [ \x.t'' | \x.t' <- t, t'' <- reduce t']
         [ Lam x t'' | Lam x t' <- term, t'' <- reduce redex t']
 
-eta :: Lambda Term
+eta :: (Monad m) => Lambda m Term
 eta = -- [ t' | \x_1.t' x_2 <- t, x_1 = x_2, not x_1 free in t']
       [ t' | Lam x_1 (App t' (X x_2)) <- term, x_1 == x_2, not $ x_1 `freein` t']
 
-alpha :: Lambda Term
+alpha :: (Monad m) => Lambda m Term
 alpha = -- [ \x_2.t'[x_1|->x_2] | \x_1.t' <- t, x_2 <- fresh]
         [ Lam x_2 t'' | Lam x_1 t' <- term, x_2 <- fresh, t'' <- (t' `subst` x_1|->X x_2)]
 
-(|->) :: Name -> Term -> Lambda Term
+(|->) :: (Monad m) => Name -> Term -> Lambda m Term
 x_s |-> t_s =
   -- [ t_s | x <- t, x = x_s ] <|>
   [ t_s | X x <- term, x == x_s ] <|>
@@ -77,5 +76,5 @@ x_s |-> t_s =
   -- [ \x'.t''[x_s|->t_s] | \x.t' <- t, x /= x_s, x free in t_s, \x'.t'' <- alpha \x.t']
   [ Lam x' t''' | Lam x t' <- term, x /= x_s, x `freein` t_s, Lam x' t'' <- reduce alpha $ Lam x t', t''' <- (t'' `subst` x_s|->t_s)]
 
-redex :: Lambda Term
+redex :: (Monad m) => Lambda m Term
 redex = eta <|> cong2 <|> beta <|> cong1 <|> cong0
--- app_lambda/Main.hs
+++ app_lambda/Main.hs
@@ -5,14 +5,54 @@ import DynSem (eval)
 
 import Text.Parsec (parse)
 import System.IO (hFlush, stdout)
+import System.Environment (getProgName, getArgs)
+import System.Exit (exitFailure)
+import Control.Monad.Reader (ReaderT, runReaderT, ask)
+import Control.Monad.State (StateT)
+import Control.Monad.Trans.Maybe (MaybeT, runMaybeT)
+import Control.Monad.Trans (liftIO, lift)
 import Control.Monad (forever)
+import Control.Monad (mzero)
+
+data Mode = NormalForm | Step
+
+usage :: IO ()
+usage = do n <- getProgName
+           putStrLn $ "Usage: " ++ n ++ " [-ns]"
+
+nf :: (Monad m) => a -> m a
+nf = return
+
+step :: (Show t) => ReaderT t (StateT a (MaybeT IO)) t
+step = do t <- ask
+          liftIO $ print t
+          lift $ lift doGoOn
+          return t
+
+doGoOn :: MaybeT IO ()
+doGoOn = do liftIO $ putStr "=> "
+            liftIO $ hFlush stdout
+            inp <- liftIO $ getLine
+            case inp of
+              x | x `elem` ["s", "step"] -> return ()
+              x | x `elem` ["q", "quit"] -> mzero
+              otherwise -> doGoOn
 
 main :: IO ()
-main = do
+main = do args <- getArgs
+          mode <- case args of
+                    [] -> return NormalForm
+                    ["-n"] -> return NormalForm
+                    ["-s"] -> return Step
+                    otherwise -> usage >> exitFailure
           forever $ do
             putStr "-> "
             hFlush stdout
             inp <- getLine
             case parse term "" inp of
               Left err -> putStrLn $ "error: " ++ show err
-              Right t -> putStrLn $ show $ eval t
+              Right t ->
+                case mode of
+                    NormalForm -> print =<< eval t nf
+                    Step -> do t' <- runMaybeT $ eval t $ runReaderT step
+                               maybe (return ()) print t'
